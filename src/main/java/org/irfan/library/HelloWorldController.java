b88a46445c8b18d593c56fe4ecc384a7b52be71c
MagicIrfan <irfan.bouhenaf@etu.mines-ales.fr>
2024-02-13 21:07:51 +0100

    Début d'implémentation de spring security

--- src/main/java/org/irfan/library/HelloWorldController.java
+++ src/main/java/org/irfan/library/HelloWorldController.java
@@ -6,8 +6,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
-    @GetMapping("/")
-    public String getHello(){
-        return "Hello world";
+    @GetMapping("/user")
+    public String getUser() {
+        return "Welcome, User";
+    }
+
+    @GetMapping("/admin")
+    public String getAdmin() {
+        return "Welcome, Admin";
     }
 }
